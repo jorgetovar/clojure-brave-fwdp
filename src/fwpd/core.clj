c2d49d7c0c6e0a1380a3367907bcd6414870de33
jorgetovar <jorgetovar621@gmail.com>
2021-03-08 17:19:38 -0500

    add append functions

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,5 +1,8 @@
-(ns fwpd.core)
-(require '[clojure.string :as cs])
+(ns fwpd.core
+    (:require [clojure.spec.alpha :as s])
+    (:require [clojure.string :as cs])
+
+    )
 (def filename "suspects.csv")
 
 (def vamp-keys [:name :glitter-index])
@@ -28,9 +31,23 @@
               {}
               (map vector vamp-keys unmapped-row)))
 
+(s/def ::vampire (s/keys :req-un [::name ::glitter-index]))
+
+(defn validate [row]
+      (s/valid? ::vampire row))
+
+(defn append [coll row]
+      (if (validate row) (conj coll row) coll))
+
 (defn glitter-filter
       [minimum-glitter records]
       (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(defn names [coll]
+      (map #(:name %) coll)
+      )
+
+
 
 (defn mapify
       "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
